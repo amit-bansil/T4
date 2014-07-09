869279842dbd0b08cebf7fdb2a10799c940d7552
Amit D. Bansil <amit@bansil.org>
2014-07-09 13:21:00 -0400

    stub for pen file

--- src/Utils.js
+++ src/Utils.js
@@ -2,47 +2,4 @@
 (function() {
   "use strict";
 
-  var utils = {};
-
-  //see wikipedia:cartesian product
-  utils.product = function(sets) {
-    var product2 = function(memos, set) {
-      var ret = _.map(memos, function(memo) {
-        return _.map(set, function(element) {
-          return memo.concat([element]);
-        });
-      });
-      return _.flatten(ret, true);
-    };
-    return _.reduce(sets, product2, [
-      []
-    ]);
-  };
-
-  utils.repeat = function(element, n) {
-    return _.map(_.range(n), function() {
-      return element;
-    });
-  };
-
-  utils.checkContains = function(array, element) {
-    if (!_.contains(array, element)) {
-      throw "" + element + " not found in " + array;
-    }
-  };
-
-  utils.nextElement = function(array, element) {
-    return array[(array.indexOf(element) + 1) % array.length];
-  };
-
-  //more defensive and less efficient version of constant
-  utils.constant = function(value) {
-    value = _.clone(value);
-    return function() {
-      return _.clone(value);
-    };
-  };
-
-  _.mixin(utils);
-
 }());
