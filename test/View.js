f1638e08ecc962262f94394fa32f588e34f18e5a
Amit D. Bansil <amit@bansil.org>
2014-07-09 13:20:18 -0400

    more view tests

--- test/View.js
+++ test/View.js
@@ -12,11 +12,13 @@
     $('body').append(canvas);
     var view = new T4.View(model, canvas);
     model.restart();
-    _.each(options.moves.split(' '), function(move) {
-      var x = parseInt(move[0], 10);
-      var y = parseInt(move[1], 10);
-      model.take(model.getSquare([x, y]));
-    });
+    if (options.moves) {
+      _.each(options.moves.split(' '), function(move) {
+        var x = parseInt(move[0], 10);
+        var y = parseInt(move[1], 10);
+        model.take(model.getSquare([x, y]));
+      });
+    }
     view.update();
   }
 
@@ -25,6 +27,15 @@
       dimensions: [6, 6],
       winLength: 4,
       moves: '00 11 01 22 10 33 02 44'
+    });
+    testView({
+      dimensions: [3, 3],
+      winLength: 3,
+      moves: '11 00 22 01 10'
+    });
+    testView({
+      dimensions: [5, 3],
+      winLength: 3
     });
   }
   $(document).ready(onReady);
