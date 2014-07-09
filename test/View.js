6cc36ae3ed419c0e19d9815292b89c244eb386a0
Amit D. Bansil <amit@bansil.org>
2014-07-09 12:58:14 -0400

    basic impl of test view

--- test/View.js
+++ test/View.js
@@ -1,9 +1,29 @@
 /*global $:true, _:true, T4:true */
 (function() {
-  var setup = {
-    dimensions: [6, 6],
-    winLength: 4
-  };
-  this.model = new T4.Model(setup);
-  this.view = new T4.View(this.model, $('#canvas'));
+  function testView(options) {
+    var setup = {
+      dimensions: options.dimensions,
+      winLength: options.winLength
+    };
+    var model = new T4.Model(setup);
+    var canvas = $('<canvas id="canvas1" width="300" height="300"></canvas>');
+    $('body').append(canvas);
+    var view = new T4.View(model, canvas);
+    model.restart();
+    _.each(options.moves.split(' '), function(move) {
+      var x = parseInt(move[0], 10);
+      var y = parseInt(move[1], 10);
+      model.take(model.getSquare([x, y]));
+    });
+    view.update();
+  }
+
+  function onReady() {
+    testView({
+      dimensions: [6, 6],
+      winLength: 4,
+      moves: '00 11 01 22 10 33 02 44'
+    });
+  }
+  $(document).ready(onReady);
 }());
